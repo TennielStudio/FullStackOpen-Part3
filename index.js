b9942aac3c7a55819f6a0d7d7c65728fd1dc1abf
Tenniel <tenniel.lynn@gmail.com>
2024-04-10 07:01:51 -0700

    save dist file

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ morgan.token('morganPost', (request, response) => {
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :morganPost'))
 app.use(cors())
+app.use(express.static('dist'))
 
 app.get('/api/persons/', (request, response) => {
     response.json(persons)
@@ -99,7 +100,7 @@ app.delete('/api/persons/:id/', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3002
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
