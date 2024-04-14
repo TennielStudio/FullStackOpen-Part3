49b0f63a5c9c870bf97aee09677c28db14b70cb6
Tenniel <tenniel.lynn@gmail.com>
2024-04-14 11:37:09 -0700

    finish part3 exercises

--- mongo.js
+++ mongo.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length <3) {
-    console.log('give password as an argument')
-    process.exit(1)
+  console.log('give password as an argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
-const argName = process.argv[3]
-const argNumber = process.argv[4]
 
 const url = `mongodb+srv://admin:${password}@cluster0.5y4t3l5.mongodb.net/part3-exercises?retryWrites=true&w=majority&appName=Cluster0`
 
@@ -16,8 +14,8 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -32,11 +30,11 @@ const Person = mongoose.model('Person', personSchema)
 //     mongoose.connection.close()
 // })
 
-if (process.argv.length == 3) {
-    Person.find({}).then(result => {
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-            mongoose.connection.close()
-        })
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+      mongoose.connection.close()
     })
+  })
 }
